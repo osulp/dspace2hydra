1d9afe46e0778a12d0d2d600e0cad16e1dbbbcdc
Josh Gum <josh.gum@oregonstate.edu>
2017-01-31 14:14:56 -0800

    update configuration and example embargo method

--- mapping/description.rb
+++ mapping/description.rb
@@ -1,5 +1,13 @@
 module Mapping
   class Description
     extend BasicValueHandler
+
+    def set_embargo(value, *args)
+      field_name_one, field_name_two = args.flatten
+      [
+        { field_name: field_name_one, value: 'embargo' },
+        { field_name: field_name_two, value: value }
+      ]
+    end
   end
 end
