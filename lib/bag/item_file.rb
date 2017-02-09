1af7f9437a8e072a8992f14fe8bf8dd5bc88869c
Josh Gum <josh.gum@oregonstate.edu>
2017-02-09 13:44:53 -0800

    create temporary file with proper filename before uploading to the server
    add spec for covering ItemFile metadata file handling

--- lib/bag/item_file.rb
+++ lib/bag/item_file.rb
@@ -1,5 +1,5 @@
 class ItemFile
-  attr_accessor :full_path
+  attr_accessor :full_path, :metadata_full_path
 
   def initialize(full_path)
     @full_path = full_path
@@ -8,15 +8,31 @@ class ItemFile
   ##
   # Return the file name without the extension
   # @returns [String] the filename without the extension
-  def file_name
-    File.basename @full_path, ".*"
+  def file_name(at_path = nil)
+    at_path ||= @full_path
+    File.basename at_path, ".*"
   end
 
   ##
   # Return a File object to the full path of this ItemFile
   # @returns [File] file object
-  def file
-    File.new @full_path
+  def file(at_path = nil)
+    at_path ||= @full_path
+    File.new at_path
+  end
+
+  ##
+  # Intends to make a temporary copy of the file using the filename from the ItemFile's metadata
+  # so that uploading into Hydra will show the proper/meaningful filename.
+  def copy_to_metadata_full_path
+    FileUtils.copy @full_path, metadata_full_path
+  end
+
+  ##
+  # Delete the temporary copy of the file and unset the class attribute
+  def delete_metadata_full_path
+    FileUtils.safe_unlink(metadata_full_path)
+    @metadata_full_path = nil
   end
 
   def description
@@ -33,6 +49,12 @@ class ItemFile
 
   def source
     @source ||= metadata_xml.xpath(CONFIG['bag']['item']['item_file']['source_xpath']).children.first.to_s
+  end
+
+  ##
+  # Within the same directory as the original file, return a full path using the filename from this ItemFile's metadata
+  def metadata_full_path
+    @metadata_full_path ||= "#{File.dirname(@full_path)}/#{name}"
   end
 
   def metadata_xml
--- spec/lib/bag/item_file_spec.rb
+++ spec/lib/bag/item_file_spec.rb
@@ -3,9 +3,14 @@ RSpec.describe ItemFile do
 
   #path to the for this "File"
   let(:full_path) { File.join(File.dirname(__FILE__), "../../fixtures/ITEM@1957-57239/data/ORIGINAL/6")}
+  let(:metadata_full_path) { File.join(File.dirname(__FILE__), "../../fixtures/ITEM@1957-57239/data/ORIGINAL/VanTuyl-Whitmire-PLOSOne-Final.pdf")}
 
   it "has a file" do
     expect(subject.file).to be_a_kind_of File
+  end
+
+  it "has a metadata_full_path" do
+    expect(subject.metadata_full_path).to eq metadata_full_path
   end
 
   it "has a full_path" do
@@ -26,6 +31,14 @@ RSpec.describe ItemFile do
   it 'has a name' do
     expect(subject.name).not_to be_nil
   end
+
+  it 'copies and deletes a temporary metadata file with proper name' do
+    subject.copy_to_metadata_full_path
+    expect(File.exists?(metadata_full_path)).to be_truthy
+    subject.delete_metadata_full_path
+    expect(File.exists?(metadata_full_path)).to be_falsey
+  end
+
   context "#metadata_xml" do
     it "has a Nokogiri XML Document" do
       expect(subject.metadata_xml).to be_a_kind_of Nokogiri::XML::Document
