81fdf909e486c9b53bbb6cd56768f2ec263e045e
Josh Gum <josh.gum@oregonstate.edu>
2017-02-14 16:00:58 -0800

    adds basic ability to exclude file regexs by directory

--- dspace2hydra.rb
+++ dspace2hydra.rb
@@ -11,7 +11,7 @@ OptionParser.new do |opts|
   opts.banner = 'Usage: dspace2hydra.rb [options]'
 
   opts.on('-b', '--bag PATH', 'The Dspace bag path to process.') { |v| options['bag_path'] = v }
-  opts.on('-c', '--config PATH', 'The Item config path for each bag.') { |v| options['mapping_config'] = v }
+  opts.on('-c', '--config PATH', 'The Item type config path for each bag.') { |v| options['type_config'] = v }
   opts.on('-d', '--directory PATH', 'The directory path containing bags to bulk process.') { |v| options['bags_directory'] = v }
   opts.on('-h', '--help', 'Display this screen') do
     puts opts
@@ -21,52 +21,52 @@ end.parse!
 
 CONFIG.merge!(options)
 raise 'Missing BAG path or BAGS directory.' if CONFIG['bag_path'].nil? && CONFIG['bags_directory'].nil?
-raise 'Missing BAG mapping config.' if CONFIG['mapping_config'].nil?
+raise 'Missing BAG type config.' if CONFIG['type_config'].nil?
 
 bags = []
-mapping_config = File.open(File.join(File.dirname(__FILE__), CONFIG['mapping_config'])) { |f| YAML.load(f) }
+type_config = File.open(File.join(File.dirname(__FILE__), CONFIG['type_config'])) { |f| YAML.load(f) }
 
 if CONFIG['bags_directory']
   # process each bag sub-directory
   Pathname.new(CONFIG['bags_directory']).children.select do |bag_path|
-    bags << Bag.new(bag_path, mapping_config) if bag_path.directory?
+    bags << Bag.new(bag_path, type_config) if bag_path.directory?
   end
 else
-  bags << Bag.new(CONFIG['bag_path'], mapping_config)
+  bags << Bag.new(CONFIG['bag_path'], type_config)
 end
 
 ## Testing loading one bag
+bag = bags.first
+
 data = {}
-bags.first.item.metadata.each do |k, nodes|
+bag.item.metadata.each do |_k, nodes|
   nodes.each do |metadata_node|
     data = metadata_node.process_node(data)
   end
 end
-bags.first.item.custom_metadata.each do |k, nodes|
+bag.item.custom_metadata.each do |_k, nodes|
   nodes.each do |custom_metadata_node|
     data = custom_metadata_node.process_node(data)
   end
 end
 
-#TODO: Show data before it loads?
-#pp data
-#exit
+# TODO: Show data before it loads?
+# pp data
+# exit
 
 he = HydraEndpoint.new(CONFIG['hydra_endpoint'])
 
-file_ids  = []
-files_to_upload = bags.first.files.select { |item_file| mapping_config['upload_data_directories'].include?(item_file.parent_directory) }
-files_to_upload.each do |item_file|
+file_ids = []
+bag.files_for_upload.each do |item_file|
   # Make a temporary copy of the file with the proper filename, upload it, grab the file_id from the servers response
   # and remove the temporary file
   item_file.copy_to_metadata_full_path
   upload_response = he.upload(item_file.file(item_file.metadata_full_path))
   json = JSON.parse(upload_response.body)
-  file_ids << json["files"].map { |f| f["id"] }
+  file_ids << json['files'].map { |f| f['id'] }
   item_file.delete_metadata_full_path
 end
 file_ids.flatten!.uniq!
 
 submitted_page = he.submit_new_work data, file_ids
 pp submitted_page
-
--- lib/bag/bag.rb
+++ lib/bag/bag.rb
@@ -1,9 +1,9 @@
 class Bag
-  attr_reader :path, :config
+  attr_reader :path, :type_config
 
-  def initialize(path, config)
+  def initialize(path, type_config)
     @path = path
-    @config = config
+    @type_config = type_config
   end
 
   def bagit
@@ -15,15 +15,23 @@ class Bag
   end
 
   def item
-    @item ||= Item.new File.join(@path, CONFIG['bag']['item']['directory']), @config
+    @item ||= Item.new File.join(@path, CONFIG['bag']['item']['directory']), @type_config
   end
 
   def data_paths
     @data_paths ||= load_data_paths
   end
 
+  def upload_configs
+    @upload_configs ||= @type_config['upload_data']
+  end
+
   def files
     @files ||= load_files
+  end
+
+  def files_for_upload
+    @files_for_upload ||= filter_upload_files
   end
 
   private
@@ -33,7 +41,7 @@ class Bag
   # @returns [Hash<String, String>] a hash of the path => hashcode for each file in the manifest
   def load_manifest
     manifest_file = File.join(@path, CONFIG['bag']['manifest_file'])
-    raise "Missing #{manifest_file}" unless File.exists? manifest_file
+    raise "Missing #{manifest_file}" unless File.exist? manifest_file
     h = {}
     File.readlines(manifest_file).each do |line|
       hash, path = line.split(' ')
@@ -46,17 +54,45 @@ class Bag
   # Load all of the paths to data files from the manifest
   # @returns [Array<String>] an array of string paths from the manifest
   def load_data_paths
-    manifest.keys.select {|key| key.match(/#{CONFIG['bag']['item']['item_file']['directory_pattern']}/) != nil }
+    manifest.keys.select { |key| !key.match(/#{CONFIG['bag']['item']['item_file']['directory_pattern']}/).nil? }
   end
 
   ##
   # Instantiate a list of ItemFile ordered by their sequence_id
   # @returns [Array<ItemFile>] an array of ItemFile objects
   def load_files
-    pattern = CONFIG['bag']['item']['item_file']['metadata_file_name_template'].gsub "{item_file_name}", ""
-    files = load_data_paths.reject {|key| key.match(/#{pattern}$/)}.map do |file_path|
+    pattern = CONFIG['bag']['item']['item_file']['metadata_file_name_template'].gsub '{item_file_name}', ''
+    files = load_data_paths.reject { |key| key.match(/#{pattern}$/) }.map do |file_path|
       ItemFile.new File.join(@path, file_path)
     end
-    files.sort_by! { |f| f.sequence_id }
+    files.sort_by!(&:sequence_id)
+  end
+
+  ##
+  # Filter the list of data files to include only files that should be uploaded
+  # based on the configuration
+  # @returns [Array<ItemFile>] an array of ItemFile objects
+  def filter_upload_files
+    upload_directories = upload_configs.map { |config| config['directory'] }
+    upload_files = files.select do |item_file|
+      in_upload_directory = upload_directories.include?(item_file.parent_directory)
+      # TODO: Add logging that this file is being ignored for upload
+      next unless in_upload_directory
+      ignored = item_file_ignored?(item_file)
+      # TODO: Add logging if this file is being ignored for upload
+      next(!ignored)
+    end
+    upload_files
+  end
+
+  ##
+  # Determing if the item_file supplied matches one of the ignore_files regex in the configuration
+  # for this item_file's parent_directory.
+  # @param [ItemFile] item_file - the item file to evalute
+  # @returns [Boolean] - true if the item_file.name is a match for any of the ignore_files configuration
+  def item_file_ignored?(item_file)
+    upload_config = upload_configs.first { |config| config['directory'].include?(item_file.parent_directory) }
+    upload_config['ignore_files'] ||= []
+    upload_config['ignore_files'].any? { |regex| item_file.name.match(/#{regex}/i) }
   end
 end
