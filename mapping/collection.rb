ad545d297e271f0edaa7dc014fa1f452a2802b65
Josh Gum <josh.gum@oregonstate.edu>
2017-04-03 15:44:07 -0700

    fixes bug in AdminSet regarding collectionlist lookup

--- mapping/collection.rb
+++ mapping/collection.rb
@@ -7,16 +7,16 @@ module Mapping
     extend Extensions::BasicValueHandler
 
     ##
-    # 
     #
-    # @param[String] value - An array of collection handles passed from CustomNode 
-    # @param[Array] *args - 
-    # return - 
-    def self.lookup_collection_name(value, *args)
+    #
+    # @param[String] value - An array of collection handles passed from CustomNode
+    # @param[Array] *args -
+    # return -
+    def self.lookup_collection_name(value, *_args)
       csv_file = File.join(File.dirname(__FILE__), '../lookup/collectionlist.csv')
       # array of hash
-      lines = CSV.read(csv_file, :headers=>true).map(&:to_hash)
-      lines.map { |line| line[:collection_name] if line[:collection_handle].casecmp(value).zero? }
+      lines = CSV.read(csv_file, headers: true).map(&:to_hash)
+      lines.select { |l| value.to_s.downcase.include? l['collection_handle'].downcase }.map { |l| l['collection_name'] }
     end
   end
 end
--- mapping/type.rb
+++ mapping/type.rb
@@ -12,7 +12,8 @@ module Mapping
     def self.lookup_hyrax_type(value, *args)
       lookup = File.open(File.join(File.dirname(__FILE__), '../lookup/resource.types.yml')) { |f| YAML.safe_load(f) }
       resource_type_map = lookup.find { |l| l['from'].casecmp(value).zero? }
-      { field_name: args, value: resource_type_map['to'] }
+      field_name = args.flatten.first
+      { field_name: field_name, value: resource_type_map['to'] }
     end
   end
 end
