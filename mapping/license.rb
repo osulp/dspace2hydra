cb92753cbd76699c1c09ed9661f37743824561f2
Josh Gum <josh.gum@oregonstate.edu>
2017-06-01 09:15:02 -0700

    sets appropriate resource type for associated ITEM degree level

--- mapping/license.rb
+++ mapping/license.rb
@@ -5,25 +5,26 @@ module Mapping
 
     ##
     # Process license based on values in date_issued and right.uri
-    # @param[Hash<String,<Array<Metadata::Node>>] value - The full hash of migration metadata with node name as the key and Metadata::Node array 
+    # @param[Hash<String,<Array<Metadata::Node>>] value - The full hash of migration metadata with node name as the key and Metadata::Node array
     # @param[Array] *args - ignored
-    
+
     # When custom_node overwrites migration_node (the same field_name), make sure the field types of qualifiers are the same (e.g., String or Array)
     # default qualifier method can overwrite node method
     def self.process_metadata(value, *_args)
-      date_issued_node = value["date"].find { |n| n.qualifier.field_name.casecmp("date_issued").zero? }
+      date_issued_node = value['date'].find { |n| n.qualifier.field_name.casecmp('date_issued').zero? }
       date_issued = date_issued_node.qualifier.run_method
-      rights_uri_node = value["rights"].find { |n| n.qualifier.field_name.casecmp("license").zero? }
+      rights_uri_node = value['rights'].find { |n| n.qualifier.field_name.casecmp('license').zero? }
+      return nil if rights_uri_node.nil?
       rights_uri = rights_uri_node.qualifier.run_method
 
       # process license
       # https://docs.google.com/spreadsheets/d/1_Mj90z_abGrmn_xz-fnM8mF_NWGDv9br7kNIs6FfQWw/edit#gid=0
 
       # return public domain as-is
-      return rights_uri if rights_uri.downcase.include?("http://creativecommons.org/publicdomain/zero/1.0/")
-     
-      # anything after 1923, with creativecommons return the rights_uri 
-      if DateTime.parse(date_issued) > DateTime.new(1923,12,31) && rights_uri.include?("creativecommons.org")
+      return rights_uri if rights_uri.downcase.include?('http://creativecommons.org/publicdomain/zero/1.0/')
+
+      # anything after 1923, with creativecommons return the rights_uri
+      if DateTime.parse(date_issued) > DateTime.new(1923, 12, 31) && rights_uri.include?('creativecommons.org')
         return rights_uri
       else
         return nil
--- mapping/type.rb
+++ mapping/type.rb
@@ -3,6 +3,9 @@ module Mapping
   class Type
     extend Extensions::BasicValueHandler
 
+    include Loggable
+    @logger = Logging.logger[self]
+
     ##
     # Use the resource.types.yml to map the original Dspace value to a resource type in Hyrax
     # @param [String] value - the original Dspace value for the node
@@ -14,6 +17,30 @@ module Mapping
       resource_type_map = lookup.find { |l| l['from'].casecmp(value).zero? }
       field_name = args.flatten.first
       { field_name: field_name, value: resource_type_map['to'] }
+    end
+
+    def self.type_for_degree_level(metadata, *_args)
+      # using the pre-processed metadata hash of nodes, grab the translated degree_level and resource_type
+      # to reset the resource_type to the appropriate value depending on which degree_level the item
+      # is set to.
+      type_node = metadata['type'].find { |n| n.qualifier.field_name.casecmp('resource_type').zero? }
+      type = type_node.qualifier.run_method
+      type_value = type.is_a?(Hash) ? type[:value] : type
+
+      degree_level_node = metadata['degree'].find { |n| n.qualifier.field_name.casecmp('degree_level').zero? }
+      return type_value if degree_level_node.nil?
+
+      degree_level = degree_level_node.qualifier.run_method
+      degree_level.delete!("'")
+
+      case type_value.downcase
+      when 'thesis'
+        return 'Dissertation' if degree_level.casecmp('doctoral').zero?
+        return 'Masters Thesis' if degree_level.casecmp('masters').zero?
+      else
+        @logger.fatal("Mapping::type_for_degree_level expected 'thesis' but found '#{type_value}' in metadata.")
+        return type_value
+      end
     end
   end
 end
